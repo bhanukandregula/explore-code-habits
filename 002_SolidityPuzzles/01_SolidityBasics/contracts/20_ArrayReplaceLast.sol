cfbaf17d99e52fba341c580dd954a59b95548b40
bhanukandregula <bhanuprakashkandregula@gmail.com>
2022-03-31 17:23:21 -0400

    Relace array specific index with last element

--- 002_SolidityPuzzles/01_SolidityBasics/contracts/20_ArrayReplaceLast.sol
+++ 002_SolidityPuzzles/01_SolidityBasics/contracts/20_ArrayReplaceLast.sol
@@ -4,7 +4,28 @@ pragma solidity ^0.8.7;
 contract ArrayReplaceLast {
     uint[] public scores;
 
-    function remove(uint _idex) public {
-        
+    // [1,2,3,4] = remove(1) = [1,4,3]
+    // replace the removed element postion with the last element
+    function remove(uint _index) public {
+        scores[_index] = scores[scores.length -1];
+        scores.pop();
+    }
+
+    function test() external {
+        scores = [1,2,3,4];
+        remove(1);
+        //[1,4,3]
+        assert(scores.length == 3);
+        assert(scores[0] == 1);
+        assert(scores[1] == 4);
+        assert(scores[2] == 3);
+
+        remove(2);
+        // [1,4]
+
+        assert(scores.length == 2);
+        assert(scores[0] == 1);
+        assert(scores[1] == 4);
+
     }
 }
