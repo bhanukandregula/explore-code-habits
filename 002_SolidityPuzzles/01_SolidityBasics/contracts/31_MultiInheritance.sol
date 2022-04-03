569aa48254692bf1aae2f9c1e81dcfdafaf8e589
bhanukandregula <bhanuprakashkandregula@gmail.com>
2022-04-03 13:08:02 -0400

    Multi inheritence implemented

--- 002_SolidityPuzzles/01_SolidityBasics/contracts/31_MultiInheritance.sol
+++ 002_SolidityPuzzles/01_SolidityBasics/contracts/31_MultiInheritance.sol
@@ -1,3 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+contract A {
+    // keyword "virtual" will tell solidity, this function can be inherited and customized 
+    function foo() public pure virtual returns (string memory) {
+        return "A";
+    }
+
+    function bar() public pure virtual returns( string memory) {
+        return "A";
+    }
+
+    function baz() public pure returns (string memory) {
+        return "A";
+    }
+}
+
+contract B is A {
+    // override is the implementation of virtual functions in inheritance
+    function foo() public pure virtual override returns (string memory) {
+        return "B";
+    }
+
+    function bar() public pure virtual override returns( string memory) {
+        return "B";
+    }
+
+    function testB() public pure returns (string memory) {
+        return "B";
+    }
+}
+
+// This is multi inheritance, since foo() and bar() are being overridden in contracts A and C
+contract C is A, B  {
+    function foo() public pure override(A, B) returns( string memory) {
+        return "C";
+    }
+
+     function bar() public pure override(A, B) returns( string memory) {
+        return "B";
+    }
+}
