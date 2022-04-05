360bf9c421e449c28d2d406cc1fdadbc66191aba
bhanukandregula <bhanuprakashkandregula@gmail.com>
2022-04-04 21:23:07 -0400

    selfdestructing the contract

--- 002_SolidityPuzzles/01_SolidityBasics/contracts/49_DeletingContract.sol
+++ 002_SolidityPuzzles/01_SolidityBasics/contracts/49_DeletingContract.sol
@@ -1,6 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+// These are the ways we can self diuctruct the contracts
+    // - delete contract
+    // - force send Ether to any address
 contract DeleteContract {
-    
+
+    constructor() payable {
+
+    }
+
+    function kill() external {
+        // force sending ether to another contract
+        selfdestruct(payable(msg.sender));
+    }
+
+    function testCall() external pure returns(uint) {
+        return 123;
+    }
+
+}
+
+contract Helper {
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+
+    function kill( DeleteContract _kill) external {
+        _kill.kill();
+    }
 }
